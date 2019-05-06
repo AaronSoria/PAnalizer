60cd5dc793404778ef1b371c6a5cf18bb08ee7f6
AaronSoria <soria_aaron@outlook.com>
2019-05-05 23:31:33 -0300

    installer added

---
+++ install_linux.py
@@ -0,0 +1,12 @@
+
+###################################################
+#  Run this script and install all dependencies.
+###################################################
+import os
+os.system('sudo apt-get install python-pip')
+os.system('sudo pip install imutils==0.5.2')
+os.system('sudo pip install matplotlib==3.0.3')
+os.system('sudo pip install numpy==1.16.2')
+os.system('sudo pip install opencv-python==4.0.0.21')
+os.system('sudo pip install opencv-contrib-python==4.0.0.21')
+os.system('sudo pip install PyQt5==5.7')
---
+++ install_windows.py
@@ -0,0 +1,10 @@
+###############################################################
+#  Run this script on windows  and install all dependencies.
+###############################################################
+import os
+os.system('C:\Python27\Scripts\pip install imutils==0.5.2')
+os.system('C:\Python27\Scripts\pip install matplotlib==3.0.3')
+os.system('C:\Python27\Scripts\pip install numpy==1.16.2')
+os.system('C:\Python27\Scripts\pip install opencv-python==4.0.0.21')
+os.system('C:\Python27\Scripts\pip install opencv-contrib-python==4.0.0.21')
+os.system('C:\Python27\Scripts\pip install PyQt5==5.7')
